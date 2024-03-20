e29b20b54b62a8f990c4a4a457c993a4174314e5
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-03-20 17:45:31 -0400

    Define and use Dynamic Routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
 
 function ProductsPage() {
-    return <h1>Product page</h1>
+  return (
+    <>
+      <h1>Product page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
 }
 
 export default ProductsPage;
